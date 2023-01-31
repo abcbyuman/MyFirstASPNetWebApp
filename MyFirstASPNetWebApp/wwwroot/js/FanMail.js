2de371255828ab337bae7547375dd2537a44115d
austi <austi@OST>
2023-01-30 23:55:06 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,19 @@
+﻿$("#btnSend").click(function () {
+
+    var final_grade;
+    console.log(document.getElementById("assignment"))
+    console.log(document.getElementById("intex"))
+
+
+    final_grade = Number(($("#assignments").val()) * .5) +
+        Number(($("#groupProject").val()) * .1) +
+        Number(($("#quizzes").val()) * .1) +
+        Number(($("#midtermExam").val()) * .1) +
+        Number(($("#finalExam").val()) * .1) +
+        Number(($("#intex").val()) * .1);
+
+    document.getElementById("output1").innerHTML = final_grade + "%";
+
+});
+
+
